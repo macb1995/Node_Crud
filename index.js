9b1f94815d9d63818679332329c7c58b35ce0c33
MACB <104180332+macb1995@users.noreply.github.com>
2022-09-03 16:51:55 -0600

    conexión a mysql
    Conexión a la base de datos

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express"
+import { conectar } from "./modelo/db_conectar.js";
 const app_e = express();
+app_e.use(express.static('./vista'))
+app_e.use(express.static('./controlador'))
+app_e.use(express.static('./modelo'))
 app_e.listen('5000', function() {
     console.log('Aplicación iniciada en: http://localhost:5000')
 })
