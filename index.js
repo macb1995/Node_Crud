d31b9a82a26d21faa1efb0d44769683a7ae2343b
MACB <104180332+macb1995@users.noreply.github.com>
2022-09-04 22:09:47 -0600

    cambios
    cambios

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const app_e = express();
 app_e.use(express.static('./vista'))
 app_e.use(express.static('./controlador'))
 app_e.use(express.static('./modelo'))
+    // motor de vistas
+app_e.set('views', './vista')
+app_e.set('view engine', 'ejs')
 app_e.listen('5000', function() {
     console.log('Aplicación iniciada en: http://localhost:5000')
 })
 app_e.get('/', function(req, res) {
-    res.send("Hola Mundo, como estan?")
+    conectar.query('select * from estudiantes', (error, results) => {
+        if (error) {
+            throw error;
+        } else res.render('estudiantes/index')
+    })
+
 })
